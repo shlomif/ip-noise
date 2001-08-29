e57813fc8d80a92912ae89e4eabd6b6b8baa2d5f
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-08-29 09:39:09 +0000

    Added some comments to IP::Noise::Arb

--- perl/arbitrator/IP/Noise/Arb.pm
+++ perl/arbitrator/IP/Noise/Arb.pm
@@ -2,6 +2,8 @@ package IP::Noise::Arb;
 
 use strict;
 
+# Usage: 
+#      my $arb = IP::Noise::Arb->new($connection_handle);
 sub new
 {
     my $class = shift;
@@ -68,31 +70,44 @@ sub loop
     {
         my $opcode = $self->read_opcode(); 
 
+        # Check if an operation for this opcode was defined.
         if (! exists($operations{$opcode}))
         {
+            # If not report it to the user immidiately.
             $conn->conn_write(pack("V", 0x2));
+            # Read the next opcode.
+            next;
         }
-
+        
+        # The record of the operation.
         my $record = $operations{$opcode};
 
         my @in_param_types = @{$record->{'params'}};
         my @params = ();
-
+        
+        # Read the parameters from the line.
         foreach my $param_type (@in_param_types)
         {
             push @params, $self->read_param_type($param_type);
         }
+        
+        # Call the handler to perform the operation.
         my ($ret_code, @ret) = 
             $record->{'handler'}->(
                 $self,
                 @params
                 );
-
+        
+        # Write the return code.
         $conn->conn_write(pack("V", $ret_code));
-
+        
+        # Write the output parameters to the line.
         if (exists($record->{'out_params'}))
         {
-            for(my $i=0;$i<scalar(@{$record->{'out_params'}});$i++)
+            my $i;
+            my $num_params = scalar(@{$record->{'out_params'}})
+            
+            for($i=0 ; $i < $num_params ; $i++)
             {
                 $self->write_param_type(
                     $record->{'out_params'}->[$i],
