8333756e4b515d8d9d780bb638976016b888588e
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-08-29 17:18:53 +0000

    Implemented more operations in the Arbitrator and the Translator.

--- perl/arbitrator/IP/Noise/Arb.pm
+++ perl/arbitrator/IP/Noise/Arb.pm
@@ -2,6 +2,10 @@ package IP::Noise::Arb;
 
 use strict;
 
+use IP::Noise;
+
+my $arb_string_len = IP::Noise::get_max_id_string_len() + 1;
+
 # Usage: 
 #      my $arb = IP::Noise::Arb->new($connection_handle);
 sub new
@@ -25,6 +29,16 @@ sub initialize
 
     $self->{'conn'} = $conn;
 
+    $self->{'data'} = {
+        'chains' => [],
+        'chains_map' => {},
+    };
+
+    # This member specifies whether to continue the loop.
+    $self->{'continue'} = 1;
+
+    $self->{'last_chain'} = -1;
+
     return 0;
 }
 
@@ -41,23 +55,166 @@ sub read_opcode
 
 my %operations =
 (
+    0x0 =>
+    {
+        'params' => [ "string" ],
+        'out_params' => [ "int" ],
+        'handler' => \&handler_new_chain,
+    },
+    0x1 =>
+    {
+        'params' => [ "chain", "string"],
+        'out_params' => [ "int" ],
+        'handler' => \&handler_new_state,
+    },
     0x19 =>
     {
         'params' => [],
         'handler' => \&handler_clear_all,
-    }
-    
+    },
+    0x10000 =>
+    {
+        'params' => [],
+        'handler' => \&handler_end_connection,
+    },
 );
 
 sub handler_clear_all
 {
     my $self = shift;
 
-    # TODO: Really clear all the chains.
+    my $data = $self->{'data'};
 
     print "Clear All!\n";
+    
+    # Delete all the chains.
+    @{$data->{'chains'}} = ();
+    %{$data->{'chains_map'}} = ();
 
     return 0;
+}
+
+sub handler_new_chain
+{
+    my $self = shift;
+
+    my $data = $self->{'data'};
+
+    # TODO: Make sure we do not allocate two chains by the same name
+
+    print "New Chain!\n";
+
+    my $name = shift;
+
+    push @{$data->{'chains'}}, 
+        { 
+            'name' => $name, 
+            'states' => [], 
+            'states_map' => {},
+        };
+    
+    my $index = scalar(@{$data->{'chains'}})-1;
+
+    $data->{'chains_map'}->{lc($name)} = $index;
+
+    $self->{'last_chain'} = $index;
+
+    $self->{'last_state'} = -1;
+
+    return (0, $index);
+}
+
+sub handler_end_connection
+{
+    my $self = shift;
+
+    $self->{'continue'} = 0;
+}
+
+sub handler_new_state
+{
+    my $self = shift;
+
+    my $chain_index = shift;
+
+    my $state_name = shift;
+
+    # TODO: Sanity check that there isn't another state by that name.
+
+    my $data = $self->{'data'};
+
+    my $chain = $data->{'chains'}->[$chain_index];
+
+    my $states_ref = $chain->{'states'};
+
+    push @$states_ref, { 'name' => $state_name };
+
+    my $index = scalar(@$states_ref) - 1;
+
+    $chain->{'states_map'}->{lc($state_name)} = $index;
+
+    $self->{'last_state'} = $index;
+
+    return (0, $index);
+}
+
+
+sub read_param_type
+{
+    my $self = shift;
+
+    my $param_type = shift;
+
+    my $conn = $self->{'conn'};
+    
+    if ($param_type eq "string")
+    {
+        my $ret = $conn->conn_read($arb_string_len);
+
+        $ret =~ s/\x00.*$//;
+
+        return $ret;
+    }
+    elsif ($param_type eq "chain")
+    {
+        my $which = $conn->conn_read(4);
+
+        $which = unpack("V", $which);
+
+        # TODO : Implement the other <Chain> types.
+        if ($which == 2)
+        {
+            return $self->{'last_chain'};
+        }
+        else
+        {
+            die "Unknown which $which!\n";
+        }
+    }
+    else
+    {
+        die "Unknown param type: $param_type!\n";
+    }
+}
+
+sub write_param_type
+{
+    my $self = shift;
+
+    my $param_type = shift;
+
+    my $value = shift;
+
+    my $conn = $self->{'conn'};
+
+    if ($param_type eq "int")
+    {
+        $conn->conn_write(pack("V", $value));
+    }
+    else
+    {
+        die "Unkown param_type \"$param_type\"!\n";
+    }
 }
 
 sub loop
@@ -66,13 +223,14 @@ sub loop
 
     my $conn = $self->{'conn'};
 
-    while(1)
+    while($self->{'continue'})
     {
         my $opcode = $self->read_opcode(); 
 
         # Check if an operation for this opcode was defined.
         if (! exists($operations{$opcode}))
         {
+            print "Uknown opcode $opcode!\n";
             # If not report it to the user immidiately.
             $conn->conn_write(pack("V", 0x2));
             # Read the next opcode.
@@ -105,7 +263,7 @@ sub loop
         if (exists($record->{'out_params'}))
         {
             my $i;
-            my $num_params = scalar(@{$record->{'out_params'}})
+            my $num_params = scalar(@{$record->{'out_params'}});
             
             for($i=0 ; $i < $num_params ; $i++)
             {
--- perl/compiler/IP/Noise/C/Translator.pm
+++ perl/compiler/IP/Noise/C/Translator.pm
@@ -2,6 +2,15 @@ package IP::Noise::C::Translator;
 
 use strict;
 
+use IP::Noise;
+
+my $arb_string_len = IP::Noise::get_max_id_string_len() + 1;
+
+sub LAST_CHAIN ()
+{
+    return { 'type' => 'last' };
+}
+
 sub new
 {
     my $class = shift;
@@ -39,6 +48,24 @@ my %transactions =
         'params' => [],
         'out_params' => [],
     },
+    'new_chain' =>
+    {
+        'opcode' => 0x0,
+        'params' => [ "string" ],
+        'out_params' => [ "int" ],
+    },
+    'end_connection' =>
+    {
+        'opcode' => 0x10000,
+        'params' => [],
+        'out_params' => [],
+    },
+    'new_state' =>
+    {
+        'opcode' => 0x01,
+        'params' => [ "chain", "string" ],
+        'out_params' => [ "int" ],
+    },
 );
 
 sub pack_int32
@@ -67,6 +94,12 @@ sub read_retvalue
     return unpack("V", $value);
 }
 
+sub pack_string
+{
+    my $string = shift;
+
+    return pack(("a" . $arb_string_len), $string);     
+}
 
 sub transact
 {
@@ -85,23 +118,54 @@ sub transact
 
     $conn->conn_write(pack_opcode($record->{'opcode'}));
 
-    foreach my $param (@{$record->{'params'}})
+    foreach my $param_type (@{$record->{'params'}})
     {
         # TODO: Write each parameter according to what was inputted in the 
         # function arguments.
+        my $param = shift;
+
+        if ($param_type eq "string")
+        {
+            $conn->conn_write(pack_string($param));
+        }
+        elsif ($param_type eq "chain")
+        {
+            if ($param->{'type'} eq "last")
+            {
+                $conn->conn_write(pack_int32(2));
+            }
+            else
+            {
+                die "Unknown chain type!\n";
+            }
+        }
+        else
+        {
+            die "Unknown param_type $param_type!\n";
+        }
     }
 
     my $ret_value = $self->read_retvalue();
 
     my (@ret);
-    foreach my $param (@{$record->{'out_params'}})
+    foreach my $param_type (@{$record->{'out_params'}})
     {
         #TODO: read output params from the line and return them.
+        if ($param_type eq "int")
+        {
+            my $int = $conn->conn_read(4);
+            push @ret, unpack("V", $int);
+        }
+        else
+        {
+            die "Unknown param_type $param_type!\n";
+        }
     }
 
     return ($ret_value, \@ret);
 }
 
+my $ws = "    ";
 
 sub load_arbitrator
 {
@@ -118,8 +182,45 @@ sub load_arbitrator
 
     foreach my $chain (values(%{$ds->{'chains'}}))
     {
-        print $chain->{'name'}, "\n";
+        # Create a new chain by that name in the arbitrator
+        print "In chain: " , $chain->{'name'}, "\n";
+
+        ($ret_value, $other_args) = 
+            $self->transact(
+                "new_chain", 
+                $chain->{'name'}
+                );
+
+        if ($ret_value != 0)
+        {
+            die "The arbitrator did not give a valid chain index!\n";
+        }
+
+        $chain->{'id'} = $other_args->[0];
+
+        print "Chain ID =  ",  $chain->{'id'} , "!\n";
+
+        foreach my $state_name (keys(%{$chain->{'states'}}))
+        {
+            print "In State " , $state_name, "\n";
+            ($ret_value, $other_args) = 
+                $self->transact(
+                    "new_state",
+                    LAST_CHAIN,
+                    $state_name
+                    );
+            
+            if ($ret_value != 0)
+            {
+                die "The arbitrator did not accept the state!\n";
+            }
+            $chain->{'states'}->{$state_name}->{'id'} = $other_args->[0];
+        }
     }
+
+    ($ret_value, $other_args) = $self->transact("end_connection");
+
+    return 0;
 }
 
 
