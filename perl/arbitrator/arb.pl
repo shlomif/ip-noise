4136c1a6954a069cef93189057eb253ee95745ed
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-09-07 07:10:33 +0000

    Integrated the other threads into the arbitrator.

--- perl/arbitrator/arb.pl
+++ perl/arbitrator/arb.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/perl -w
 
+use strict;
+
 use Data::Dumper;
+use Thread;
+use Thread::Queue;
+use Thread::RWLock;
+
+use IPTables::IPv4::IPQueue qw(:constants);
+use Time::HiRes qw(usleep gettimeofday);
 
 use IP::Noise::Arb::IFace;
-
 use IP::Noise::Arb::Switcher;
-
 use IP::Noise::Conn;
 
-use Thread;
-
-use Thread::RWLock;
+use IP::Noise::Arb::Delayer;
+use IP::Noise::Arb::Packet::Logic;
 
 use vars qw(%flags $data $data_lock $arb_iface $update_states);
 
@@ -45,8 +50,113 @@ sub thread_func_update_states
 
 my $thread_update_states = Thread->new(\&thread_func_update_states);
 
+my $packget_logic = IP::Noise::Arb::Packet::Logic->new();
 
-#my $d = Data::Dumper->new([ $data ],  [ '$data' ]);
+my $ip_queue = IPTables::IPv4::IPQueue->new(
+    'copy_mode' => IPQ_COPY_PACKET,
+    'copy_range' => 2048
+    );
 
-#print $d->Dump();
+if (! $ip_queue)
+{
+    die IPTables::IPv4::IPQueue->errstr();
+}
+
+# Thread::Queue is a class for a thread-safe queue.
+my $packets_to_arbitrate_queue = Thread::Queue->new();
+
+my $release_callback = 
+    sub {
+        my $msg = shift;
+
+        $queue->set_verdict($msg->packet_id, NF_ACCEPT);
+    };
+
+my $delayer = Delayer->new($release_callback);
+
+my $terminate = 0;
+
+sub release_packets_thread_func
+{
+    while (! $terminate)
+    {
+        {
+            lock($delayer);
+            $delayer->release_packets_poll_function();
+        }
+        usleep(500);
+    }
+}
+
+sub decide_what_to_do_with_packets_thread_func
+{
+    my ($verdict);
+
+    while (! $terminate)
+    {
+        my $msg_with_time = $packets_to_arbitrate_queue->dequeue();
+
+        my $msg = $msg_with_time->{'msg'};
+        
+        $verdict = $packet_logic->decide_what_to_do_with_packet($msg);
+
+        if ($verdict->{'action'} eq "accept")
+        {
+            # Accept immidiately
+            $release_callback->($msg);
+        }
+        elsif ($verdict->{'action'} eq "drop")
+        {
+            # Drop the packet
+            $queue->set_verdict($msg->packet_id, NF_DROP);
+        }
+        elsif ($verdict->{'action'} eq "delay")
+        {
+            # Delay the packet for $verdict quanta
+            lock($delayer);
+            $delayer->delay_packet(
+                $msg,
+                $msg_with_time->{'sec'},
+                $msg_with_time->{'usec'},
+                $msg_with_time->{'index'},
+                $verdict->{'delay_len'},
+                );
+        }
+        else
+        {
+            $terminate = 1;
+            die "Unknown Action!\n";
+        }
+    }
+}
+
+my $release_packets_thread_handle = Thread->new(\&release_packets_thread_func);
+my $decide_what_to_do_with_packets_thread_handle = Thread->new(\&decide_what_to_do_with_packets_thread_func);
+
+my ($last_sec, $last_usec) = (0, 0);
+my $packet_index = 0;
+while (1)
+{
+    my $msg = $ip_queue->get_message();
+    my ($sec, $usec) = gettimeofday();
+
+    # We have to do something so it won't overflow...
+    $packet_index++;
+
+    if (! $msg)
+    {
+        $terminate = 1;
+        die IPTables::IPv4::IPQueue->errstr();
+    }
+
+    $packets_to_arbitrate_queue->enqueue(
+        {
+            'msg' => $msg,
+            'sec' => $sec,
+            'usec' => $usec,
+            'index' => $packet_index,
+        }
+        );
+}
+
 
