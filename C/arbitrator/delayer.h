751fcc2b1269cd556bdcb02d5d1c5e5eac3754f9
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-09-08 13:49:03 +0000

    Added another thread for handling delayed packets.

---
+++ C/arbitrator/delayer.h
@@ -0,0 +1,28 @@
+
+#ifndef __IP_NOISE_DELAYER_H
+#define __IP_NOISE_DELAYER_H
+
+#include <linux/netfilter.h>
+#include <libipq.h>
+#include <sys/time.h>
+
+struct ip_noise_delayer_struct
+{
+    
+};
+
+typedef struct ip_noise_delayer_struct ip_noise_delayer_t;
+
+ip_noise_delayer_t * ip_noise_delayer_alloc(void);
+
+void ip_noise_delayer_delay_packet(
+    ip_noise_delayer_t * delayer, 
+    ipq_packet_msg_t * m,
+    struct timeval tv,
+    int delay_len
+    );
+
+void ip_noise_delayer_poll(
+    ip_noise_delayer_t * delayer
+    );
+#endif /* #ifndef __IP_NOISE_DELAYER_H */
--- C/arbitrator/main.c
+++ C/arbitrator/main.c
@@ -7,6 +7,8 @@
 #include <string.h>
 
 #include "queue.h"
+#include "delayer.h"
+#include "verdict.h"
 
 
 static void die(struct ipq_handle * h)
@@ -16,11 +18,40 @@ static void die(struct ipq_handle * h)
     exit(-1);
 }
 
+
+ip_noise_verdict_t decide_what_to_do_with_packet(ipq_packet_msg_t * m)
+{
+    int what_to_do;
+    int delay;
+    ip_noise_verdict_t ret;
+
+    what_to_do = (rand() % 3);
+    delay = (rand() % 3000);
+
+    if (what_to_do == 0)
+    {
+        ret.action = IP_NOISE_VERDICT_ACCEPT;
+    }
+    else if (what_to_do == 1)
+    {
+        ret.action = IP_NOISE_VERDICT_DROP;
+    }
+    else
+    {
+        ret.action = IP_NOISE_VERDICT_DELAY;
+        ret.delay_len = delay;
+    }
+
+    return ret;
+}
+
+
 struct ip_noise_decide_what_to_do_with_packets_thread_context_struct
 {
     ip_noise_messages_queue_t * queue;
     int * terminate;
     struct ipq_handle * h;
+    ip_noise_delayer_t * delayer;
 };
 
 typedef struct ip_noise_decide_what_to_do_with_packets_thread_context_struct ip_noise_decide_what_to_do_with_packets_thread_context_t;
@@ -34,12 +65,17 @@ void * ip_noise_decide_what_to_do_with_packets_thread_func (void * void_context)
     int status;
     struct ipq_handle * h;
     static int num;
+    ip_noise_verdict_t verdict;
+    ip_noise_delayer_t * delayer;
 
     context = (ip_noise_decide_what_to_do_with_packets_thread_context_t * )void_context;
 
     packets_to_arbitrate_queue = context->queue;
     terminate = context->terminate;
     h = context->h;
+    delayer = context->delayer;
+
+    free(context);
 
     while (! (*terminate))
     {
@@ -49,16 +85,47 @@ void * ip_noise_decide_what_to_do_with_packets_thread_func (void * void_context)
             usleep(500);
             continue;
         }
-        
-        
 
-        printf("Release Packet! (%i)\n", num++);
-        status = ipq_set_verdict(h, msg_with_time->m->packet_id, NF_ACCEPT, 0, NULL);
-
-        if (status < 0)
+        verdict = decide_what_to_do_with_packet(msg_with_time->m);
+        
+        if (verdict.action == IP_NOISE_VERDICT_ACCEPT)
         {
-            die(h);
+            printf("Release Packet! (%i)\n", num++);
+            status = ipq_set_verdict(h, msg_with_time->m->packet_id, NF_ACCEPT, 0, NULL);
+
+            if (status < 0)
+            {
+                die(h);
+            }
         }
+        else if (verdict.action == IP_NOISE_VERDICT_DROP)
+        {
+            printf("Dropping Packet! (%i)\n", num++);
+            status = ipq_set_verdict(h, msg_with_time->m->packet_id, NF_DROP, 0, NULL);
+
+            if (status < 0)
+            {
+                die(h);
+            }
+        }
+        else if (verdict.action == IP_NOISE_VERDICT_DELAY)
+        {
+            printf("Delaying Packet! (%i)\n", num++);
+            ip_noise_delayer_delay_packet(
+                delayer,
+                msg_with_time->m,
+                msg_with_time->tv,
+                verdict.delay_len
+                );
+        }
+        else
+        {
+            *terminate = 1;
+            fprintf(stderr, "Unknown Action!\n");
+            return NULL;
+        }
+        
+        
 
         free(msg_with_time);
     }
@@ -66,6 +133,34 @@ void * ip_noise_decide_what_to_do_with_packets_thread_func (void * void_context)
     return NULL;
 }
 
+struct ip_noise_release_packets_thread_context_struct
+{
+    int * terminate;
+    ip_noise_delayer_t * delayer;
+};
+
+typedef struct ip_noise_release_packets_thread_context_struct ip_noise_release_packets_thread_context_t;
+
+void * release_packets_thread_func(void * void_context)
+{
+    ip_noise_release_packets_thread_context_t * context;
+    ip_noise_delayer_t * delayer;
+    int * terminate;
+
+    context = (ip_noise_release_packets_thread_context_t *)void_context;
+    delayer = context->delayer;
+    terminate = context->terminate;
+
+    free(context);
+
+    while (! (*terminate) )
+    {
+        ip_noise_delayer_poll(delayer);
+    }
+
+    return NULL;
+    
+}
 
 int main(int argc, char * argv[])
 {
@@ -74,9 +169,16 @@ int main(int argc, char * argv[])
     struct ipq_handle * h;
     ip_noise_messages_queue_t * packets_to_arbitrate_queue;
     int terminate = 0;
+    
     ip_noise_decide_what_to_do_with_packets_thread_context_t * arbitrator_context;
     pthread_t decide_what_to_with_packets_thread;
+    
+    ip_noise_release_packets_thread_context_t * release_packets_context;
+    pthread_t release_packets_thread;
     int check;
+    ip_noise_delayer_t * delayer;
+
+    srand(24);
 
     h = ipq_create_handle(0);
     if (h == NULL)
@@ -92,11 +194,19 @@ int main(int argc, char * argv[])
     }
 
     packets_to_arbitrate_queue = ip_noise_messages_queue_alloc();
+
+    delayer = ip_noise_delayer_alloc();
     
     arbitrator_context = malloc(sizeof(ip_noise_decide_what_to_do_with_packets_thread_context_t));
     arbitrator_context->queue = packets_to_arbitrate_queue ;
     arbitrator_context->h = h;
     arbitrator_context->terminate = &terminate;
+    arbitrator_context->delayer = delayer;
+
+    release_packets_context = malloc(sizeof(ip_noise_release_packets_thread_context_t));
+    release_packets_context->delayer = delayer;
+    release_packets_context->terminate = &terminate;
+    
 
     check = pthread_create(
         &decide_what_to_with_packets_thread,
@@ -111,6 +221,20 @@ int main(int argc, char * argv[])
         exit(-1);
     }
 
+    check = pthread_create(
+        &release_packets_thread,
+        NULL,
+        release_packets_thread_func,
+        (void *)release_packets_context
+        );
+
+    if (check != 0)
+    {
+        fprintf(stderr, "Could not create the release packets thread!\n");
+        exit(-1);
+    }
+
+    
     do
     {
         status = ipq_read(h, message, sizeof(message), 0);
