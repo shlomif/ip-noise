fa9b13737620b87bc42dbc56e96a024aa1a144f6
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-09-09 06:25:03 +0000

    ZThread-derived R/W Lock is now operational. Looks good.

--- C/arbitrator/tests/locks/zthread/rwlock.c
+++ C/arbitrator/tests/locks/zthread/rwlock.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 struct ip_noise_rwlock_struct
 {
@@ -66,14 +68,14 @@ void ip_noise_rwlock_free(ip_noise_rwlock_t * lock)
 {
     pthread_mutex_destroy(&(lock->mutex_lock));
     pthread_cond_destroy(&(lock->cond_read));
-    pthread_cond_destory(&(lock->cond_write));
+    pthread_cond_destroy(&(lock->cond_write));
 
     free(lock);
 }
 
 int ip_noise_rwlock_allow_reader(ip_noise_rwlock_t * lock)
 {
-    return (lock->active_writers == 0);
+    return (lock->active_writers == 0) && (lock->waiting_writers == 0);
 }
 
 int ip_noise_rwlock_allow_writer(ip_noise_rwlock_t * lock)
@@ -81,37 +83,210 @@ int ip_noise_rwlock_allow_writer(ip_noise_rwlock_t * lock)
     return ((lock->active_writers == 0) && (lock->active_readers == 0));
 }
 
-void ip_noise_rw_lock_before_read(ip_noise_rwlock_t * lock)
+void ip_noise_rwlock_before_read(ip_noise_rwlock_t * lock)
 {
-   int ret;
+    int ret = 0;
    
-   pthread_mutex_lock(&(lock->mutex));
+    pthread_mutex_lock(&(lock->mutex_lock));
 
-   waiting_readers++;
+    lock->waiting_readers++;
 
-   ret = pthread_cond_wait(&(lock->cond_read), &(lock->mutex));
+    while (! ip_noise_rwlock_allow_reader(lock))
+    {
 
-   if (ret == 0)
-   {
-       active_readers++;
-   }
+        ret = pthread_cond_wait(&(lock->cond_read), &(lock->mutex_lock));
 
-   waiting_readers--;
+        if (ret != 0)
+        {
+            break;
+        }
+    }
 
-   pthread_mutex_unlock(&(lock->mutex));
+    lock->waiting_readers--;
+
+    if (ret == 0)
+    {
+        lock->active_readers++;
+    }
+
+    pthread_mutex_unlock(&(lock->mutex_lock));
 }
 
-void ip_noise_rw_lock_before_write(ip_noise_rwlock_t * lock)
+void ip_noise_rwlock_before_write(ip_noise_rwlock_t * lock)
 {
+    int ret = 0;
+    
+    pthread_mutex_lock(&(lock->mutex_lock));
+
+    lock->waiting_writers++;
+
+    while (! ip_noise_rwlock_allow_writer(lock) )
+    {
+        ret = pthread_cond_wait(&(lock->cond_write), &(lock->mutex_lock));
+        if (ret != 0)
+        {
+            break;
+        }
+    }
+
+    lock->waiting_writers--;
+    if (ret == 0)
+    {
+        lock->active_writers++;
+    }
+    
+    pthread_mutex_unlock(&(lock->mutex_lock));
 }
 
-void ip_noise_rw_lock_after_read(ip_noise_rwlock_t * lock)
+void ip_noise_rwlock_after_read(ip_noise_rwlock_t * lock)
 {
+    pthread_mutex_lock(&(lock->mutex_lock));
+
+    lock->active_readers--;
+
+    lock->fairness_counter--;
+
+    pthread_cond_broadcast(&(lock->cond_write));
+
+    pthread_cond_signal(&(lock->cond_read));
+
+    pthread_mutex_unlock(&(lock->mutex_lock));
 }
 
-void ip_noise_rw_lock_after_write(ip_noise_rwlock_t * lock)
+void ip_noise_rwlock_after_write(ip_noise_rwlock_t * lock)
 {
+    pthread_mutex_lock(&(lock->mutex_lock));
+
+    lock->active_writers--;
+    lock->fairness_counter++;
+
+    /* Notify all */
+    pthread_cond_broadcast(&(lock->cond_write));
+
+    pthread_cond_signal(&(lock->cond_read));
+    
+    pthread_mutex_unlock(&(lock->mutex_lock));
+}
+
+#if 1
+
+ip_noise_rwlock_t * mylock;
+
+#define NUM_READERS 5
+#define NUM_WRITERS 2
+
+struct context_struct
+{
+    int writer;
+    int index;
+};
+
+typedef struct context_struct context_t;
+
+void * reader_thread(void * void_context)
+{
+    context_t * context;
+
+    context = (context_t *)void_context;
+
+    while (1)
+    {
+        ip_noise_rwlock_before_read(mylock);
+
+        printf("Reader %i - Lock!\n", context->index);
+
+        usleep(rand()%1000000);
+
+        ip_noise_rwlock_after_read(mylock);
+
+        printf("Reader %i - Unlock!\n", context->index);
+
+        usleep(rand()%1000000);
+    }
+
+    return NULL;
+}
+
+void * writer_thread(void * void_context)
+{
+    context_t * context;
+
+    context = (context_t *)void_context;
+
+    while (1)
+    {
+        ip_noise_rwlock_before_write(mylock);
+
+        printf("Writer %i - Lock!\n", context->index);
+
+        usleep(rand()%1000000);
+
+        printf("Writer %i - Unlock!\n", context->index);
+
+        ip_noise_rwlock_after_write(mylock);
+
+        usleep(rand()%1000000);
+    }
+
+    return NULL;
 }
 
 
+int main(int argc, char * argv[])
+{    
+    context_t * context;
+    pthread_t readers[NUM_READERS];
+    pthread_t writers[NUM_WRITERS];
+    int check;
+
+    int a;
+
+    mylock = ip_noise_rwlock_alloc();
+    for(a=0;a<NUM_READERS;a++)
+    {
+        context = malloc(sizeof(context));
+        context->index = a;
+        context->writer = 0;
+        check = pthread_create(
+            &readers[a],
+            NULL,
+            reader_thread,
+            context
+            );
+        
+        if (check != 0)
+        {
+            fprintf(stderr, "Could not create Reader #%i!\n", a);
+            exit(-1);
+        }
+    }
+
+    for(a=0;a<NUM_WRITERS;a++)
+    {
+        context = malloc(sizeof(context));
+        context->index = a;
+        context->writer = 0;
+        check = pthread_create(
+            &writers[a],
+            NULL,
+            writer_thread,
+            context
+            );
+        
+        if (check != 0)
+        {
+            fprintf(stderr, "Could not create Reader #%i!\n", a);
+            exit(-1);
+        }
+    }
+
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
+
+
+#endif
 
