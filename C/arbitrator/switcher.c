77e13b988a1f45c18f6c64642df2fcc25a2720fc
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-09-14 05:41:10 +0000

    Added the switcher main loop.

--- C/arbitrator/switcher.c
+++ C/arbitrator/switcher.c
@@ -1,5 +1,6 @@
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "switcher.h"
 
@@ -45,10 +46,10 @@ static int ip_noise_timeval_cmp (void * p_m1, void * p_m2, void * context)
 }
 
 ip_noise_arbitrator_switcher_t * ip_noise_arbitrator_switcher_alloc(
-        ip_noise_arbitrator_data_t * data,
-        ip_noise_flags_t * flags,
-        int * terminate_ptr
-        )
+    ip_noise_arbitrator_data_t * data,
+    ip_noise_flags_t * flags,
+    int * terminate_ptr
+    )
 {
     ip_noise_arbitrator_switcher_t * self;
 
@@ -62,4 +63,58 @@ ip_noise_arbitrator_switcher_t * ip_noise_arbitrator_switcher_alloc(
     PQueueInitialise(&(self->pq), 30, 0, ip_noise_timeval_cmp, NULL);
 
     return self;
+}
+
+void ip_noise_arbitrator_switcher_loop(
+    ip_noise_arbitrator_switcher_t * self
+    )
+{
+    ip_noise_flags_t * flags;
+    ip_noise_arbitrator_data_t * data;
+    ip_noise_rwlock_t * data_lock;
+    ip_noise_arbitrator_switcher_event_t current_time_pseudo_msg, * event;
+    int chain_index;
+
+    flags = self->flags;
+    data = self->data;
+    data_lock = data->lock;
+    
+    while (! *(self->terminate_ptr))
+    {
+        ip_noise_rwlock_down_read(data_lock);
+
+        if (flags->reinit_switcher)
+        {
+            printf("Switcher : reinit()!\n");
+            reinit(self);
+            flags->reinit_switcher = 0;
+        }
+        gettimeofday(&(current_time_pseudo_msg.tv));
+
+        
+        while (!PQueueIsEmpty(&(self->pq)))
+        {
+            event = PQueuePeekMinimum(&(self->pq));
+            if (ip_noise_timeval_cmp (event, &current_time_pseudo_msg) < 0)
+            {
+                PQueuePop(&(self->pq));
+                chain_index = event->chain;
+                free(event);
+                
+                switch_chain(self, chain_index);
+                PQueuePush(
+                    &(self->pq), 
+                    get_new_switch_event(self, chain_index)
+                    );
+            }
+            else
+            {
+                break;
+            }
+        }        
+
+        ip_noise_rwlock_up_read(data_lock);
+
+        usleep(50000);
+    }
 }
