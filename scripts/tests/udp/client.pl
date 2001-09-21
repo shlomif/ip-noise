217734d2cffcf416200f298b410aa8428aa9c257
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-09-21 11:16:23 +0000

    Added the demo UDP client and server.

---
+++ scripts/tests/udp/client.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+use strict;
+use Socket;
+use Sys::Hostname;
+
+
+
+
+my ( $count, $hisiaddr, $hispaddr, $histime,
+     $host, $iaddr, $paddr, $port, $proto,
+     $rin, $rout, $rtime, $SECS_of_70_YEARS);
+
+
+
+
+$SECS_of_70_YEARS      = 2208988800;
+
+
+
+
+$iaddr = gethostbyname(hostname());
+$proto = getprotobyname('udp');
+#$port = getservbyname('time', 'udp');
+$port = 5000;
+$paddr = sockaddr_in(0, $iaddr); # 0 means let kernel pick
+
+
+
+
+socket(SOCKET, PF_INET, SOCK_DGRAM, $proto)   || die "socket: $!";
+bind(SOCKET, $paddr)                          || die "bind: $!";
+
+$host = "localhost";
+
+$| = 1;
+$count = 0;
+
+$hisiaddr = inet_aton($host)    || die "unknown host";
+$hispaddr = sockaddr_in($port, $hisiaddr);
+while (1)
+{
+    $count++;
+    my $msg = pack("A40", sprintf("%s", $count));
+    print "Sending \"$msg\"!\n";
+    defined(send(SOCKET, $msg, 0, $hispaddr))    || die "send $host: $!";
+}
---
+++ scripts/tests/udp/server.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+use strict;
+use Socket;
+use Sys::Hostname;
+
+
+
+
+my ( $count, $hisiaddr, $hispaddr, $histime,
+     $host, $iaddr, $paddr, $port, $proto,
+     $rin, $rout, $rtime, $SECS_of_70_YEARS);
+
+
+
+
+$SECS_of_70_YEARS      = 2208988800;
+
+
+
+
+$iaddr = gethostbyname(hostname());
+$proto = getprotobyname('udp');
+$port = getservbyname('time', 'udp');
+$paddr = sockaddr_in(5000, INADDR_ANY); # 0 means let kernel pick
+
+
+
+socket(SOCKET, PF_INET, SOCK_DGRAM, $proto)   || die "socket: $!";
+bind(SOCKET, $paddr)                          || die "bind: $!";
+
+$rin = '';
+vec($rin, fileno(SOCKET), 1) = 1;
+
+
+# timeout after 10.0 seconds
+while (select($rout = $rin, undef, undef, 200.0)) {
+    $rtime = '';
+    ($hispaddr = recv(SOCKET, $rtime, 40, 0))        || die "recv: $!";
+    print "Received Message: \"$rtime\"!\n";
+}
+
+
