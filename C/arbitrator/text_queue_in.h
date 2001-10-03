3bfe7d93baf6345c2b7895aa0b2e8bde0e582568
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-10-03 18:15:09 +0000

    Added the text_queue_in.h file.

---
+++ C/arbitrator/text_queue_in.h
@@ -0,0 +1,45 @@
+#ifndef __IP_NOISE_TEXT_QUEUE_IN_H
+#define __IP_NOISE_TEXT_QUEUE_IN_H
+
+struct ip_noise_text_queue_in_struct
+{
+    char * buffer;
+    int length;
+    int max_size;
+    int ptr_offset;
+    int is_conn_closed;
+};
+
+typedef struct ip_noise_text_queue_in_struct ip_noise_text_queue_in_t;
+
+extern ip_noise_text_queue_in_t * ip_noise_text_queue_in_alloc(void);
+
+extern void ip_noise_text_queue_in_destroy(
+    ip_noise_text_queue_in_t * q
+    );
+
+extern void ip_noise_text_queue_in_put_bytes(
+    ip_noise_text_queue_in_t * q,
+    char * bytes,
+    int length
+    );
+
+extern int ip_noise_text_queue_in_read_bytes(
+    ip_noise_text_queue_in_t * q,
+    char * dest,
+    int num_bytes
+    );
+
+extern void ip_noise_text_queue_in_set_conn_closed(
+    ip_noise_text_queue_in_t * q
+    );
+
+extern void ip_noise_text_queue_in_commit(
+    ip_noise_text_queue_in_t * q
+    );
+
+extern void ip_noise_text_queue_in_rollback(
+    ip_noise_text_queue_in_t * q
+    );
+
+#endif /* #ifndef __IP_NOISE_TEXT_QUEUE_IN_H */
