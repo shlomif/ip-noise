04508262e8066634975de346b403310ca6fe3820
shlomif <shlomif@c1b7c130-d56e-0410-b82b-b372f08e5f9e>
2001-11-15 01:11:17 +0000

    Removed an #ifdef that just initialized an ip_noise_rand variable.

--- C/arbitrator/main.c
+++ C/arbitrator/main.c
@@ -391,24 +391,7 @@ ip_noise_arbitrator_packet_logic_t * main_init_module(
 
     return packet_logic;
     
-#endif
-
-
-    
-
-#if 0
-    {
-        ip_noise_rand_t * rand;
-
-        rand = ip_noise_rand_alloc(24);
-    
-        while(1)
-        {
-            printf("%f\n", ip_noise_rand_rand_in_0_1(rand));
-            sleep(1);
-        }
-    }
-#endif
+#endif  
 
 #ifndef __KERNEL__
     do
